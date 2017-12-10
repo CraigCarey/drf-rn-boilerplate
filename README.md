0374df5a1c92949c314f55e073d1ea81072f4018
craig2 <craig2@Craigs-MacBook.local>
2017-12-10 17:42:21 +0000

    Initial commit. Basic server functionality

---
+++ README.md
@@ -0,0 +1,55 @@
+# Django Rest Framework / React Native Boilerplate
+
+REST API & mobile client with basic auth behaviour
+
+DRF Server based on [Mark Winterbottom's Udemy course](https://www.udemy.com/django-python)
+
+## Server
+
+### Requirements
+* Vagrant
+* virtualbox
+
+### Setting up Vagrant box (on the host machine):
+```
+$ vagrant init     # creates a config file
+$ vagrant up
+$ vagrant ssh
+$ vagrant halt
+$ vagrant destroy
+
+```
+
+Files in the same directory as the Vagrantfile are synced to `/vagrant/`
+
+### Setting up a virtual environment (on the Vagrant box):
+```
+$ mkvirtualenv profiles_api --python=python3
+$ deactivate
+$ workon profiles_api
+```
+
+### Install requirements
+```
+$ pip install django==1.11
+$ pip install djangorestframework==3.6.2
+```
+
+### Start Django project
+```
+$ mkdir /vagrant/src/ && cd /vagrant/src/
+$ django-admin.py startproject profiles_project
+$ cd profiles_project
+$ python manage.py startapp profiles_api
+```
+
+### Start Django Development Server
+```
+$ python manage.py makemigrations
+$ python manage.py migrate
+$ python manage.py createsuperuser
+$ python manage.py runserver 0.0.0.0:8080 # listen on all IP Addresses
+```
+
+## Client
+TODO
---
+++ server/src/profiles_project/profiles_api/views.py
@@ -0,0 +1,46 @@
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from . import serializers
+from . import models
+from . import permissions
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profiles"""
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
+
+
+class LoginViewSet(viewsets.ViewSet):
+    """Checks email and password and return an auth token"""
+
+    serializer_class = AuthTokenSerializer
+
+    def create(self, request):
+        """Use the ObtainAuthToken APIView to validate and create a token"""
+
+        return ObtainAuthToken().post(request)
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items"""
+
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.PostOwnStatus, IsAuthenticatedOrReadOnly)
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user"""
+
+        serializer.save(user_profile=self.request.user)
